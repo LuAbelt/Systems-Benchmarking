9f9a4fb32495659d2855b54cc6d0b78f172024f6
Lukas Abelt <git@login.luabelt.de>
2021-09-13 23:31:11 +0200

    Fixed path in commit benchmark script

--- scripts/benchmark_commits.sh
+++ scripts/benchmark_commits.sh
@@ -4,13 +4,13 @@
 #SBATCH --partition=seminar
 #SBATCH --constraint=kine
 #SBATCH --array=0-6
-#SBATCH --time=02:00:00
-#SBATCH --output=/scratch/abeltluk/slurm_logs/lz4commits/%A/commit_benchmark_%A_%a.out
-#SBATCH --error=/scratch/abeltluk/slurm_logs/lz4commits/%A/commit_benchmark_%A_%a.err
+#SBATCH --time=12:00:00
+#SBATCH --output=/scratch/abeltluk/slurm_logs/lz4commits/commit_benchmark_%A_%a.out
+#SBATCH --error=/scratch/abeltluk/slurm_logs/lz4commits/commit_benchmark_%A_%a.err
 
 USER=abeltluk
 USER_BASE_DIR=/scratch/$USER
-RELEASES_FILE=$USER_BASE_DIR/lz4commits
+RELEASES_FILE=$USER_BASE_DIR/SystemsBenchmarking/benchmark/lz4commits
 
 RELEASES_LIST=($(<$RELEASES_FILE))
 BENCH_BASE_DIR=$USER_BASE_DIR/SystemsBenchmarking/commit_benchmark
