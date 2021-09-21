fe074bda0f06a67513b698670fc285d56e1d7901
Lukas Abelt <l.abelt@luabelt.de>
2021-09-21 12:52:43 +0200

    Minor updates to uild script and visualizations

--- code/visualization/main.py
+++ code/visualization/main.py
@@ -2,12 +2,12 @@ import csv
 import os
 
 import matplotlib.pyplot as plt
-#from signal_processing_algorithms.energy_statistics import energy_statistics
+from signal_processing_algorithms.energy_statistics import energy_statistics
 import numpy.random
 
 
 def plot_release_data():
-    release_res_dir = "../../results/releases_benchmark/"
+    release_res_dir = "../../release_benchmark/mintime/results"
     labels = []
     compression_speed = []
     decompression_speed = []
@@ -22,21 +22,29 @@ def plot_release_data():
             compression_speed.append(float(line[1]))
             decompression_speed.append(float(line[2]))
 
-    plt.plot(labels, compression_speed,label="Compression speed")
+    decompression_speed.sort(key=dict(zip(decompression_speed,labels)).get)
+    compression_speed.sort(key=dict(zip(compression_speed,labels)).get)
+    labels.sort()
+
+    plt.plot(labels, decompression_speed,label="Decompression speed",color="orange")
     plt.ylabel('MB/s')
     plt.xlabel("Version")
-    plt.ylim([0, 600.0])
+    plt.ylim([0, 4000.0])
     plt.xticks(rotation=90)
+
+    #plt.clf()
+    plt.plot(labels, compression_speed, label="Compression speed")
     plt.legend()
     plt.tight_layout()
-    plt.savefig("compression_plot.png")
+    plt.savefig("releases_plot.png")
 
 
 def plot_commit_data():
-    commit_res_dir = "../../results/commit_benchmark/"
-    commit_hash_file = "../../results/lz4commits_hashs"
+    commit_res_dir = "../../commit_benchmark/mintime/results/"
+    commit_hash_file = "../../benchmark/merge_commits"
     compression_speed = []
     decompression_speed = []
+    labels = []
 
     with open(commit_hash_file) as hash_file:
         for commit_hash in hash_file:
@@ -44,6 +52,7 @@ def plot_commit_data():
                 continue
             with open(os.path.join(commit_res_dir,commit_hash.strip()+".res")) as commit_file:
                 reader = csv.reader(commit_file)
+                labels.append(commit_hash)
                 next(reader)
                 next(reader)
                 line = next(reader)
@@ -59,10 +68,26 @@ def plot_commit_data():
         plt.xlabel("# of commits since v1.8.1")
         plt.ylim([0,4000])
         plt.legend(loc=9)
-        #plt.axvline(x=3,color="r",lw=0.5,ls=":")
-        #plt.axvline(x=313,color="r",lw=0.5, ls=":")
+
+        # Calculate change points based on created data
+        comp_cp = energy_statistics.e_divisive(compression_speed, pvalue=0.1, permutations=100)
+        decomp_cp = energy_statistics.e_divisive(decompression_speed, pvalue=0.1, permutations=100)
+
+        print(comp_cp)
+        print(decomp_cp)
+
+        for cp in comp_cp:
+            plt.vlines(x=cp,color="r",lw=0.5,ls=":",ymin=250,ymax=750)
+
+        # plt.scatter(comp_cp,[compression_speed[cp] for cp in comp_cp],marker='.',c='r')
+        # plt.scatter(decomp_cp,[decompression_speed[cp] for cp in decomp_cp],marker='.',c='r')
+
+        for cp in decomp_cp:
+            plt.vlines(x=cp,color="g",lw=0.5, ls=":",ymax=max(decompression_speed),ymin=min(decompression_speed))
         plt.tight_layout()
-        plt.savefig("commits.png")
+        labels.reverse()
+        print(labels[decompression_speed.index(max(decompression_speed))])
+        plt.show()
 
 
 def create_outlier():
@@ -86,6 +111,6 @@ def create_changepoint():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    plot_release_data()
+    plot_commit_data()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
